1916305f51eecb01fb91fc43e4ba1308f1fcdcb7
neledov <neledov@gmail.com>
2021-04-19 15:35:02 +0200

    removed sensitive files

--- bugbuddy_apiserver.py
+++ bugbuddy_apiserver.py
@@ -10,13 +10,15 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+
 class bug(Resource):
-    def get(self,description_text):
-        description_text = description_text.replace('description_text=','')
-        a = findingoogle(description_text.replace('%20','* '))
-        print description_text.replace('%20','* ')
+    def get(self, description_text):
+        description_text = description_text.replace('description_text=', '')
+        a = findingoogle(description_text.replace('%20', '* '))
         a = list(dict.fromkeys(a))
-        my_new_list = ['['+x + "]("+ "https://quickview.cloudapps.cisco.com/quickview/bug/" + x + ") : " + gettitle(x) + '&#13;' for x in a]
+        my_new_list = [
+            '[' + x + "](" + "https://quickview.cloudapps.cisco.com/quickview/bug/" + x + ") : " + gettitle(x) + '&#13;'
+            for x in a]
         return jsonify(my_new_list)
 
 
@@ -25,14 +27,11 @@ def gettitle(bugid):
 
     retitle = re.compile(r'<title>Cisco Bug: .{13}([\S\s]*?)</title>', re.MULTILINE)
     title = re.findall(retitle, page.text)
-    # print title
     final = []
     if not title:
         final.insert(0, " - No information avaliable externally")
     else:
         final = list(filter(None, title))
-        print final[0]
-    print "gettitle"
     return final[0]
 
 
@@ -43,19 +42,16 @@ def fstr(sourcelist):
 def findingoogle(usertext):
     search = re.compile(r'(?=CSC[\S\s]).{10}', re.MULTILINE)
     a = google.search("""Cisco Bug """ + usertext, 5)
-    print a
     answer = re.findall(search, fstr(a))
     if answer:
         ##answer.insert(0, "*Possible bugs for your request* : ")
-        print answer
+        print(answer)
     else:
         answer.insert(0, "Nothing found, please be more exact")
-    print "findingoogle"
     return answer[:10]
 
 
 def getdescr(bugid):
-
     page = requests.get("https://quickview.cloudapps.cisco.com/quickview/bug/" + bugid)
     retitle = re.compile(r'<title>Cisco Bug: ([\S\s]*?)</title>', re.MULTILINE)
     resymptom = re.compile(r'<B>Symptom:</B>([\S\s]*?)<B>Conditions:</B>', re.MULTILINE)
@@ -67,7 +63,6 @@ def getdescr(bugid):
     if not symptom:
         final.insert(0, "No information avaliable")
     else:
-        print "bug found " + bugid
         symptom.insert(0, "Title : " + title[0])
         symptom.insert(1, "Quickview URL : https://quickview.cloudapps.cisco.com/quickview/bug/" + bugid)
         symptom.insert(2,
@@ -76,9 +71,8 @@ def getdescr(bugid):
         condition.insert(0, "Condition :")
         result = symptom + condition
         final = list(filter(None, result))
-        print "getdescr"
-        print final
     return final
 
-api.add_resource(bug,"/bug/<string:description_text>")
-app.run(host='0.0.0.0',debug=True)
+
+api.add_resource(bug, "/bug/<string:description_text>")
+app.run(host='0.0.0.0', debug=True)
